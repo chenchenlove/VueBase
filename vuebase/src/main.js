d3c8d1980849cc792858d148b262af78819ee356
chenchenlove <726759161@qq.com>
2022-05-12 17:41:29 +0800

    add ant-design-vue

--- vuebase/src/main.js
+++ vuebase/src/main.js
@@ -2,7 +2,7 @@
  * @Author: tfs\chenchen chenchen@cabrtech.com
  * @Date: 2022-05-12 09:29:03
  * @LastEditors: tfs\chenchen chenchen@cabrtech.com
- * @LastEditTime: 2022-05-12 14:22:37
+ * @LastEditTime: 2022-05-12 17:41:00
  * @FilePath: \vuebase\src\main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,8 +10,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 
-import './styles/index.scss'
+import ant from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 
+import './styles/index.scss'
+Vue.use(ant)
 Vue.config.productionTip = false
 
 new Vue({
